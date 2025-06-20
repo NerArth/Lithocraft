a32fc40ecf93a595a3a67a20695a873c89b20d1f
NerArth <notorp@gmail.com>
2025-06-20 01:03:20 +0100

    Chemistry module initial setup.

--- Modules/Lithocraft-Chem/LithocraftChemModSystem.cs
+++ Modules/Lithocraft-Chem/LithocraftChemModSystem.cs
@@ -10,5 +10,21 @@ namespace Lithocraft.Chem
 {
     public class LithocraftChemModSystem : ModSystem
     {
+        public override void Start(ICoreAPI api)
+        {
+            //base.Start(api);
+        }
+        public override void StartServerSide(ICoreServerAPI api)
+        {
+            Mod.Logger.Event(Mod.Info.Name + " " + Mod.Info.Version + " is starting (server)...");
+            //Mod.Logger.Notification("Hello from template mod server side: " + Lang.Get("lithocraft:hello"));
+        }
+
+        public override void StartClientSide(ICoreClientAPI api)
+        {
+            Mod.Logger.Event(Mod.Info.Name + " " + Mod.Info.Version + " is starting (client)...");
+            Mod.Logger.StoryEvent("Creating interactions... ");
+            //Mod.Logger.Notification("Hello from template mod client side: " + Lang.Get("lithocraft:hello"));
+        }
     }
 }
--- Modules/Lithocraft-Chem/changelog.md
+++ Modules/Lithocraft-Chem/changelog.md
@@ -3,4 +3,8 @@
 
 ## v0.0.0
 ### Overview:
-First release of the Medieval Industry: Chem module. 
+First release of the Medieval Industry: Chem module.
+
+This first release so far includes the following features, which were previously part of the Core module:
+
+- **Chemical Processes**: A variety of chemical processes that can be used to create new or vanilla materials and items.
